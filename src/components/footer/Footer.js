8ee113bfda1c35a80114b09f04b8b0b2783da153
myahyaayyas <92872659+myahyaayyas@users.noreply.github.com>
2023-06-23 19:56:30 +0700

    Footer.js

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,31 +1,29 @@
-import "./footer.module.css";
+import styles from "./footer.module.css";
 
 export function Footer() {
   return (
-    <div>
-      <header>
-        <div>
-          <h1>Covid ID</h1>
-          Developed by aufaroot18
-        </div>
-        <nav>
-          <ul>
-            <li>
-              <a href=".">Global</a>
-            </li>
-            <li>
-              <a href=".">Indonesia</a>
-            </li>
-            <li>
-              <a href=".">Provinsi</a>
-            </li>
-            <li>
-              <a href=".">About</a>
-            </li>
-          </ul>
-        </nav>
-      </header>
-    </div>
+    <footer calssName={styles.footer}>
+      <div>
+        <h1>Covid ID</h1>
+        Developed by aufaroot18
+      </div>
+      <nav>
+        <ul>
+          <li>
+            <a href=".">Global</a>
+          </li>
+          <li>
+            <a href=".">Indonesia</a>
+          </li>
+          <li>
+            <a href=".">Provinsi</a>
+          </li>
+          <li>
+            <a href=".">About</a>
+          </li>
+        </ul>
+      </nav>
+    </footer>
   );
 }
 
